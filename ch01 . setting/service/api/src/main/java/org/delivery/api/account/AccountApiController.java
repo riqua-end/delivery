2d75d7295fa834866d009bd5d4c400c7bb71c8fc
Kim KJ <riqua1@naver.com>
2024-03-12 02:47:57 +0900

    배달 플랫폼 프로젝트 -1. ObjectMapperConfig 설정 -2. ObjectMapper를 커스텀하여 애플리케이션 전반에 ObjectMapper를 주입받아 사용 -3. 모듈,역직렬화,직렬화,Naming 설정

--- ch01 . setting/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
+++ ch01 . setting/service/api/src/main/java/org/delivery/api/account/AccountApiController.java
@@ -1,11 +1,14 @@
 package org.delivery.api.account;
 
 import lombok.RequiredArgsConstructor;
+import org.delivery.api.account.model.AccountMeResponse;
 import org.delivery.db.account.AccountEntity;
 import org.delivery.db.account.AccountRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/account")
@@ -14,9 +17,13 @@ public class AccountApiController {
 
     private final AccountRepository accountRepository;
 
-    @GetMapping("")
-    public void save(){
-        var account = AccountEntity.builder().build();
-        accountRepository.save(account);
+    @GetMapping("/me")
+    public AccountMeResponse me(){
+
+        return AccountMeResponse.builder()
+                .name("홍길동")
+                .email("a@naver.com")
+                .registeredAt(LocalDateTime.now())
+                .build();
     }
 }
