ceb6bc56b2952314e56235f1da434d1bbe5ceb31
Kim KJ <riqua1@naver.com>
2024-03-17 18:07:31 +0900

    배달 플랫폼 프로젝트 -1. UserEntity create -2. UserRepository create -3. enum 클래스 UserStatus create

--- ch02 . userDomain/service/db/src/main/java/org/delivery/db/BaseEntity.java
+++ ch02 . userDomain/service/db/src/main/java/org/delivery/db/BaseEntity.java
@@ -4,13 +4,17 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 @Data
 @SuperBuilder
 @MappedSuperclass // 엔티티 상속 매핑: 부모 클래스에 매핑 정보를 선언하여 자식 클래스에 상속
+@NoArgsConstructor
+@AllArgsConstructor
 public class BaseEntity {
 
     @Id
