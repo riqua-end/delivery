6c2361fba847e2c86aba63195083ff7c8ed2a9a2
Kim KJ <riqua1@naver.com>
2024-03-18 18:16:37 +0900

    배달 플랫폼 프로젝트 -1. build.gradle jjwt 라이브러리 추가 -2. application.yaml jwt token secret key, access-token, refresh-token 설정 -3. TokenHelperInterface ( Access Token, Refresh Token, validationTokenWithThrow ) -4. JwtTokenHelper 에서 TokenHelperInterface 구현 -5. TokenErrorCode 에러 코드 설정 -6. TokenDto 반환

---
+++ ch02 . userDomain/service/api/src/main/java/org/delivery/api/domain/token/helper/JwtTokenHelper.java
@@ -0,0 +1,118 @@
+package org.delivery.api.domain.token.helper;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import jakarta.validation.Valid;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.error.TokenErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.api.domain.token.ifs.TokenHelperInterface;
+import org.delivery.api.domain.token.model.TokenDto;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+
+@Component // Spring 컨테이너에서 관리하는 Bean 으로 등록
+public class JwtTokenHelper implements TokenHelperInterface {
+    // application.yaml 의 키 : 값을 @Value("${}") 로 주입
+    @Value("${token.secret.key}")
+    private String secretKey;
+    @Value("${token.access-token.plus-hour}")
+    private Long accessTokenPlusHour;
+    @Value("${token.refresh-token.plus-hour}")
+    private Long refreshTokenPlusHour;
+
+    /**
+     * Access Token 발급 메서드
+     */
+    @Override
+    public TokenDto issueAccessToken(Map<String, Object> data) {
+        // 1. 현재 시간 + 토큰의 만료 시간 계산
+        var expiredLocalDateTime = LocalDateTime.now().plusHours(accessTokenPlusHour);
+        var expiredAt = Date.from(expiredLocalDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        // 2. 토큰 서명을 위한 키 생성
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        // 3.JWT 토큰 생성
+        var jwtToken = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256) // 토큰의 서명 생성
+                .setClaims(data) // 사용자 정보 등 토큰에 담을 데이터 설정
+                .setExpiration(expiredAt) // 토큰의 만료 시간 설정
+                .compact();
+
+        // 4. TokenDto 객체 생성 및 반환
+        return TokenDto.builder()
+                .token(jwtToken)
+                .expiredAt(expiredLocalDateTime)
+                .build();
+    }
+
+    /**
+     * RefreshToken 발급 메서드 ( AccessToken 메서드와 비슷 )
+     */
+    @Override
+    public TokenDto issueRefreshToken(Map<String, Object> data) {
+        // refreshTokenPlusHour 로 변경
+        var expiredLocalDateTime = LocalDateTime.now().plusHours(refreshTokenPlusHour);
+
+        var expiredAt = Date.from(expiredLocalDateTime.atZone(ZoneId.systemDefault()).toInstant());
+
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        var jwtToken = Jwts.builder()
+                .signWith(key, SignatureAlgorithm.HS256)
+                .setClaims(data)
+                .setExpiration(expiredAt)
+                .compact();
+
+        return TokenDto.builder()
+                .token(jwtToken)
+                .expiredAt(expiredLocalDateTime)
+                .build();
+    }
+
+    /**
+     * 토큰 검증 메서드
+     */
+    @Override
+    public Map<String, Object> validationTokenWithThrow(String token) {
+
+        // 1. 토큰 서명 검증을 위한 키 생성
+        var key = Keys.hmacShaKeyFor(secretKey.getBytes());
+
+        // 2. 토큰 파서 생성
+        var parser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+
+        try {
+            // 3. 토큰 파싱 및 정보 추출
+            var result = parser.parseClaimsJws(token);
+            // 4. 토큰 정보를 Map 으로 변환하여 반환
+            return new HashMap<String, Object>(result.getBody());
+        }
+        catch (Exception e){
+            if (e instanceof SignatureException){
+                // 토큰이 유효하지 않을 때 예외 처리
+                throw new ApiException(TokenErrorCode.INVALID_TOKEN, e);
+            } else if (e instanceof ExpiredJwtException) {
+                // 만료된 토큰의 예외 처리
+                throw new ApiException(TokenErrorCode.EXPIRED_TOKEN, e);
+            }
+            else {
+                // 그 외 에러
+                throw new ApiException(TokenErrorCode.TOKEN_EXCEPTION, e);
+            }
+        }
+
+    }
+}
