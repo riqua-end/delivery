20cda49631c13f44988cfe29a8942b7ee1cd05ea
Kim KJ <riqua1@naver.com>
2024-03-20 14:34:22 +0900

    배달 플랫폼 프로젝트 ** store domain **
    1. StoreOpenApiController : 가맹점 등록 엔드포인트 설정 및 validation
    StoreApiController : 등록된 가맹점 조회
    2. StoreBusiness : StoreConverter 를 통해 request -> entity -> response 변환
    가맹점 등록 , 카테고리로 스토어 조회
    3. StoreService : 유효한 스토어 가져오기, 스토어 등록, 카테고리로 스토어 검색, 전체 스토어 로직 -> DB에 저장
    4. StoreRegisterRequest : 스토어 등록시 request 요청 클래스
    5. StoreResponse : 스토어 응답 클래스

---
+++ ch03 . storeDomain/service/api/src/main/java/org/delivery/api/domain/store/service/StoreService.java
@@ -0,0 +1,56 @@
+package org.delivery.api.domain.store.service;
+
+import lombok.RequiredArgsConstructor;
+import org.delivery.api.common.error.ErrorCode;
+import org.delivery.api.common.exception.ApiException;
+import org.delivery.db.store.StoreEntity;
+import org.delivery.db.store.StoreRepository;
+import org.delivery.db.store.enums.StoreCategory;
+import org.delivery.db.store.enums.StoreStatus;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class StoreService {
+
+    private final StoreRepository storeRepository;
+
+    // 1. 유효한 스토어 가져오기
+    public StoreEntity getStoreWithThrow(Long id){
+
+        var entity = storeRepository.findFirstByIdAndStatusOrderByIdDesc(id, StoreStatus.REGISTERED);
+
+        return entity.orElseThrow(()->new ApiException(ErrorCode.NULL_POINT));
+    }
+    // 2. 스토어 등록
+    public StoreEntity register(StoreEntity storeEntity){
+
+        return Optional.ofNullable(storeEntity)
+                .map(it->{
+
+                    it.setStar(0);
+                    it.setStatus(StoreStatus.REGISTERED);
+                    // TODO 등록 일시 추가하기
+
+                    return storeRepository.save(it);
+                })
+                .orElseThrow(()->new ApiException(ErrorCode.NULL_POINT));
+    }
+    // 3. 카테고리로 스토어 검색
+    public List<StoreEntity> searchByCategory(StoreCategory storeCategory){
+
+        var list = storeRepository.findAllByStatusAndCategoryOrderByStarDesc(StoreStatus.REGISTERED,storeCategory);
+
+        return list;
+    }
+    // 4. 전체 스토어
+    public List<StoreEntity> registerStore(){
+
+        var list = storeRepository.findAllByStatusOrderByIdDesc(StoreStatus.REGISTERED);
+
+        return list;
+    }
+}
