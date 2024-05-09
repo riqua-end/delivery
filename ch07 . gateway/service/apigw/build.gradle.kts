12b91d5ce517fc69c475e4dde2093a96991ce854
Kim KJ <riqua1@naver.com>
2024-05-09 16:42:47 +0900

    배달 플랫폼 프로젝트 ** Spring Cloud API Gateway **
    1. gateway filter 구현
    2. account server 를 통한 인증 실행
    3. 토큰 유효성 검사
    
    토큰 검증 로직만 구현

--- ch07 . gateway/service/apigw/build.gradle.kts
+++ ch07 . gateway/service/apigw/build.gradle.kts
@@ -25,6 +25,9 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     implementation("org.springframework.cloud:spring-cloud-starter-gateway")
+
+    implementation(project(":common"))
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.projectreactor:reactor-test")
 }
--- ch07 . gateway/service/apigw/src/main/kotlin/com/delivery/apigw/filter/ServiceApiPrivateFilter.kt
+++ ch07 . gateway/service/apigw/src/main/kotlin/com/delivery/apigw/filter/ServiceApiPrivateFilter.kt
@@ -1,9 +1,22 @@
 package com.delivery.apigw.filter
 
+import com.delivery.apigw.account.model.TokenDto
+import com.delivery.apigw.account.model.TokenValidationRequest
+import com.delivery.apigw.account.model.TokenValidationResponse
 import com.delivery.apigw.common.Log
+import org.delivery.common.error.TokenErrorCode
+import org.delivery.common.exception.ApiException
 import org.springframework.cloud.gateway.filter.GatewayFilter
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory
+import org.springframework.core.ParameterizedTypeReference
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
+import org.springframework.web.reactive.function.client.ClientResponse
+import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.util.UriComponentsBuilder
+import reactor.core.publisher.Mono
 
 @Component
 class ServiceApiPrivateFilter : AbstractGatewayFilterFactory<ServiceApiPrivateFilter.Config>(Config::class.java){
@@ -19,9 +32,61 @@ class ServiceApiPrivateFilter : AbstractGatewayFilterFactory<ServiceApiPrivateFi
 
             log.info("service api private filter route uri : {}", uri)
 
-            val mono = chain.filter(exchange)
+            // account server 를 통한 인증 실행
+            // 1. 토큰의 유무
+            val headers = exchange.request.headers["authorization-token"]?: listOf()
 
-            mono
+            val token = if (headers.isEmpty()) {
+                throw ApiException(TokenErrorCode.AUTHORIZATION_TOKEN_NOT_FOUND)
+            } else {
+                headers.get(0)
+            }
+            log.info("authorization token : {}" , token)
+
+            // 2. 토큰 유효성
+            val accountApiUrl = UriComponentsBuilder
+                .fromUriString("http://localhost")
+                .port(8383) // account server port
+                .path("/internal-api/token/validation")
+                .build()
+                .encode()
+                .toUriString()
+
+            val webClient = WebClient.builder().baseUrl(accountApiUrl).build()
+
+            val request = TokenValidationRequest(
+                tokenDto = TokenDto(
+                    token = token,
+                )
+            )
+            webClient
+                .post()
+                .body(Mono.just(request), object : ParameterizedTypeReference<TokenValidationRequest>(){})
+                .accept(MediaType.APPLICATION_JSON)
+                .retrieve()
+                .onStatus(
+                    { status: HttpStatusCode? -> status?.isError == true },
+                    {
+                        response: ClientResponse -> response.bodyToMono( object: ParameterizedTypeReference<Any>(){})
+                        .flatMap { error ->
+                            log.error("", error)
+                            Mono.error(ApiException(TokenErrorCode.TOKEN_EXCEPTION))
+                        }
+                    }
+                )
+                .bodyToMono(object: ParameterizedTypeReference<TokenValidationResponse>(){})
+                .flatMap { response ->
+                    // 응답이 왔을 때
+                    log.info("response : {} ", response)
+
+                    // 3. 사용자 정보 추가
+
+
+
+                    val mono = chain.filter(exchange)
+
+                    mono
+                }
         }
     }
 }
